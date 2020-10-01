6d181724f59055cdb717d307f091eb57d894d34b
Sjors Polman <s.polman@nightcyclus.nl>
2020-10-01 13:24:24 +0200

    displaying video using react-player

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
+import Video from './components/Video.js'
+import Timestamps from './components/Timestamps.js';
 import './App.scss';
 
 function App() {
+
   return (
     <div className="App">
-      <video className="App-video" width="800" controls>
-        <source src="http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4" type="video/mp4"/>
-        We're sory, but your browser does not support HTML video.
-      </video>
-      <div className="App-sidebar">
-        <h1>Timestamps</h1>
-        <ul className="App-timestamps">
-          <li>
-            <span className="Font-light">20 seconds </span>
-            Goal
-          </li>
-        </ul>
-      </div>
+      <Video />
+      <Timestamps />
     </div>
   );
 }
