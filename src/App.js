817be24e01d63d2830df22fa6c13ad9fc7d3d347
Sjors Polman <s.polman@nightcyclus.nl>
2020-10-01 10:22:14 +0200

    Basic styling in SCSS and video setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video className="App-video" width="800" controls>
+        <source src="http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4" type="video/mp4"/>
+        We're sory, but your browser does not support HTML video.
+      </video>
+      <div className="App-sidebar">
+        <h1>Timestamps</h1>
+        <ul className="App-timestamps">
+          <li>
+            <span className="Font-light">20 seconds </span>
+            Goal
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
